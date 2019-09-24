f4ec4c5abd2afee3dfe563b33f5bc30d34cb2c43
misaad <30695712+misaad@users.noreply.github.com>
2019-09-24 17:24:38 -0400

    Add files via upload

---
+++ Deploy-CFNStack.ps1
@@ -0,0 +1,10 @@
+﻿##Update the template variab/le as well as the stack name before excuting the script.
+## Written Saad Iqbal Date:9/22/19
+## Purpose Demo
+ 
+Set-ExecutionPolicy bypass
+Import-Module AWSPowerShell
+Initialize-AWSDefaultConfiguration
+Set-AWSCredential -ProfileName SaadProfile
+$template = Get-Content -Path 'C:\Saad\AWS\Cloud Formation Templates\LampStack-Yaml-ALV2-AdminDesktop.yml' -raw
+New-CFNStack -StackName LampStack -TemplateBody $template
